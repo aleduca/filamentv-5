c7dae895057162ae5fa1b19289b0fac1efcb563d
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2026-05-07 08:18:42 -0300

    Rich editor

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -35,43 +35,34 @@ class PostForm
 					->readonly()
 					->unique('posts', 'slug', ignoreRecord:true)
 					->required(),
-				// RichEditor::make('content')
-				// ->toolbarButtons([
-				// 	['bold', 'italic', 'underline', 'highlight'],
-				// 	['textColor', 'attachFiles'], [ToolbarButtonGroup::make('Paragraph', ['paragraph', 'h1', 'h2', 'h3'])],
-				// 	[ToolbarButtonGroup::make('Alignment', ['alignStart', 'alignCenter', 'alignEnd', 'alignJustify'])],
-				// 	['codeBlock', 'bulletList', 'orderedList'],
-				// 	['undo', 'redo'],
-				// ])
-				// ->resizableImages()
-				// ->fileAttachmentsDisk('public')
-				// ->fileAttachmentsDirectory('attachments')
-				// ->fileAttachmentsVisibility('public')
-				// 			->textColors([
-				// 				'#ef4444' => 'Red',
-				// 				'#10b981' => 'Green',
-				// 				'#0ea5e9' => 'Sky',
-				// 			])
-				// // ->floatingToolbars([
-				// // 	'paragraph' => [
-				// // 		'bold', 'italic', 'underline', 'strike', 'subscript', 'superscript',
-				// // 	],
-				// // 	'heading' => [
-				// // 		'h1', 'h2', 'h3',
-				// // 	],
-				// // 	'table' => [
-				// // 		'tableAddColumnBefore', 'tableAddColumnAfter', 'tableDeleteColumn',
-				// // 		'tableAddRowBefore', 'tableAddRowAfter', 'tableDeleteRow',
-				// // 		'tableMergeCells', 'tableSplitCell',
-				// // 		'tableToggleHeaderRow', 'tableToggleHeaderCell',
-				// // 		'tableDelete',
-				// // 	],
-				// // ])
-				// 	->extraAttributes([
-				// 		'style' => 'min-height: 300px',
-				// 	])
-				// data['content']
-				TinyEditor::make('content')
+				RichEditor::make('content')
+				->toolbarButtons([
+					['bold', 'italic', 'underline', 'highlight'],
+					['textColor', 'attachFiles'], [ToolbarButtonGroup::make('Paragraph', ['paragraph', 'h1', 'h2', 'h3'])],
+					[ToolbarButtonGroup::make('Alignment', ['alignStart', 'alignCenter', 'alignEnd', 'alignJustify'])],
+					['codeBlock', 'bulletList', 'orderedList'],
+					['undo', 'redo'],
+				])
+				->resizableImages()
+				->fileAttachmentsDisk('public')
+				->fileAttachmentsDirectory('attachments')
+				->fileAttachmentsVisibility('public')
+							->textColors([
+								'#ef4444' => 'Red',
+								'#10b981' => 'Green',
+								'#0ea5e9' => 'Sky',
+							])
+				// ->floatingToolbars([
+				// 	'paragraph' => [
+				// 		'bold', 'italic', 'underline', 'strike', 'subscript', 'superscript',
+				// 	],
+				// 	'heading' => [
+				// 		'h1', 'h2', 'h3',
+				// 	],
+					->extraAttributes([
+						'style' => 'min-height: 300px',
+					])
+				// TinyEditor::make('content')
 				->required()
 				->columnSpanFull(),
 				Toggle::make('published')
